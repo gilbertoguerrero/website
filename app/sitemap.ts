137891fdcb5d88e5f03bb5a5790b5f61ba40c922
Gilberto Guerrero <12345678+gilbertoguerrero@users.noreply.github.com>
2026-03-17 22:04:10 -0500

    Page diagnostico

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,6 +10,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: `${baseUrl}/diagnostico`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
   ];
 }
 
